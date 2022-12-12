04761967bdcfb94d5ba57b84e06d3ee5646a31b8
Carlosjw <carlos.ti.infor@gmail.com>
2022-12-12 15:08:28 -0300

    updateLesson

--- 01-react-for-beginners/05-ambiente-curso/exerc01/README.md
+++ 01-react-for-beginners/05-ambiente-curso/exerc01/README.md
@@ -1,4 +1,5 @@
 # Exercício
     * Mostre os dados da aplicação como apresentado no vídeo;
     * Nâo utilize CSS externo, use o `style` para mudar as cores;
-    * Se a situação estiver ativa, pinte de verde, inativa, vermelho
+    * Se a situação estiver ativa, pinte de verde, inativa, vermelho;
+    * Se o gasto for maior que 10000 mostre uma mensagem.
--- 01-react-for-beginners/05-ambiente-curso/exerc01/src/App.js
+++ 01-react-for-beginners/05-ambiente-curso/exerc01/src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
-function App() {
+const luana = {
+  cliente: 'Luana',
+  idade:27,
+  compras: [
+    {nome: 'Notebook', preco: 'R$ 2500'},
+    {nome: 'Geladeira', preco: 'R$ 3000'},
+    {nome: 'Smartphone', preco: 'R$ 1500'},
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    {nome: 'Notebook', preco: 'R$ 2500'},
+    {nome: 'Geladeira', preco: 'R$ 3000'},
+    {nome: 'Smartphone', preco: 'R$ 1500'},
+    {nome: 'Guitarra', preco: 'R$ 3500'},
+  ],
+  ativa: false,
+}
+
+const styleActivo = {
+    color: 'green'
+}
+
+const styleInativo = {
+    color: 'red'
+}
+
+const App = () => {
+  const dados = luana;
+
   return (
     <div className="App">
-      Olá
+      <p style={styleInativo}>Teste</p>
     </div>
   );
 }
